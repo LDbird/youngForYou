95caf467d91ff384fabf04721045b3b5a15d0b2b
mabin <1164664451@qq.com>
2019-04-08 16:33:13 +0800

    正则基础

--- formular/week2/day5/2-中括号中的一些细节.js
+++ formular/week2/day5/2-中括号中的一些细节.js
@@ -1,45 +1,49 @@
 /*
-* 中括号中的一些细节问题
-*   [xyz]
-*   [^xyz]
-*   [a-z]
-*   [^a-z]
-*   1. 中括号中大多数元字符都是字符本身含义，而不是代表特殊元字符
-*   2. 中括号中的两位数不代表两位数字，而是代表两个数字当中的一个
-*
-*
-*
+* 中括号的一些细节
+* [xyz]
+* [^xyz]
+* [a-z]
+* [^a-z]
+* 1. 中括号中出现的元字符一般都是代表本身含义
+* 2. 中括号中出现的两位数不是两位数，而是两个数字中的任意一个
 *
 * */
 
-// let reg = /^.+$/; // 有^和$表示只能是xxx
-// console.log(reg.test('1')); // true
+// let reg = /^.+$/; // 一个正则如果设置了^和$，那么代表的含义是：只能是xxx
+//
 // console.log(reg.test('n')); // true
-// console.log(reg.test('_')); // true
-// console.log(reg.test('...'));  // true
+// console.log(reg.test('1')); // true
+// console.log(reg.test('nn')); // true
+// console.log(reg.test('\n')); // false 正则中的元字符.表示除 \n 以外的任意字符
 
-
-// let reg = /^[.]+$/; // 有^和$表示只能是xxx
-// console.log(reg.test('1')); // false
+// let reg = /^[.]$/;
+//
 // console.log(reg.test('n')); // false
-// console.log(reg.test('_')); // false
-// console.log(reg.test('_')); // false
-// console.log(reg.test('...'));  // true
+// console.log(reg.test('1')); // false
+// console.log(reg.test('nn')); // false
+// console.log(reg.test('\n')); // false
+// console.log(reg.test('...')); // true 
+// // 出现在方括号中的点 [.] 不再表示除\n 以外的任意字符了，而是表示一个普通的点
 
 // let reg = /^[18]$/;
+// // => 写在方括号([]) 中的两个数字不是表示一个两位数，而是表示这两个数字中的一个，所以这个正则的意思是：以一个数字1或8开头并且以一个数字1或者8结尾（想一个数字既是开头又是结尾，说明这个只能有一个，而且这个数字只能是1或者8）
+// console.log(reg.test('18')); // false
+// console.log(reg.test('1')); // true
+// console.log(reg.test('8')); // true
 
 
-// let reg = /^[12-65]$/;
+let reg = /^[12-65]$/;
+console.log(reg.test('13')); // false
+console.log(reg.test('7')); // false
+// 这个正则不是表示 12-65 而是表示 1或者2-6或者5
 
 
+// => 需求：输入的值要在 18 - 65岁
 /*
-* 编写一个正则从18-65岁
-* 18-19 1[89]
-* 20-59 [2-5]\d
-* 60-65 6[0-5]
+* [18-65] 不是代表18-65;方括号里面的两个数字，不是代表一个两位数，而是代表两个数字的当中的任意一个，如果想表示一个两位数或者多位数就要把他们拆分成多个一位数字，
+* 1[89]
+* [2-5][0-9]
+* 6[0-5]
+*
 *
 * */
-
-let reg = /^((1[89])|([2-5]\d)|(6[0-5]))$/;
-
-
