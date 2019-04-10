bd929092f2f85063e3ad660d126266e0d73f0131
mabin <1164664451@qq.com>
2019-04-10 18:19:07 +0800

    '瀑布流'

--- formular/week3/day5/js/index.js
+++ formular/week3/day5/js/index.js
@@ -1,111 +1,104 @@
 let flowRender = (function () {
-  let listBox = document.getElementById('flowBox');
-  let flowList = listBox.getElementsByTagName('li');
-  let isRun = false;
-  let page = 0;
-  let count = 0;
+  // 获取元素
+  let list = document.getElementById('flowBox');
+  let flowList = document.getElementsByTagName('li');
 
   // 获取数据
   let queryData = function () {
-    if (isRun) return;
-    isRun = true;
-
-    // => 每次请求新数据都把page累加，这样真实项目中从服务器获取的数据就是不一样的（加载10页就不在加载，提示没有更多数据，具体页数根据项目需求来定）
-    if (page > 10) {
-      alert('没有更多数据了');
-      window.onscroll = null;
-      return
-    }
-    page++;
-
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `json/data.json?page=${page}`, false);
+    xhr.open('GET', 'json/data.json', false);
     xhr.onreadystatechange = function () {
       if (+xhr.readyState === 4 && +xhr.status === 200) {
-        // do sth to insert the day to each li
         let data = JSON.parse(xhr.responseText);
-        insertHTML(data)
+
+        // 数据拿到了，接下来需要绑定数据
+        insertHTML(data);
       }
     };
     xhr.send(null);
   };
 
-
-  let queryHTML = function ({id, pic, link, title}) {
-    return `<a href="${link}" target="_blank">
+  // 生成HTML字符串
+  let queryHMTL = function ({id, title, link, pic}) {
+    return `<a href="${link}">
       <div><img data-src="${pic}" alt=""></div>
       <span>${title}</span>
     </a>`
   };
 
-  // => 为三个li进行排序，按照内容的高度进行排序
   let sortLi = function () {
     let flowListAry = Array.from(flowList);
 
+    // 按照li的实际内容高度排序，分为两种情况吧
+    // 1. 页面初次加载时，每个li都是空的，这时不用排，直接返回即可
+    // 2. 如果不是初次加载，按照每个li的实际高度，是不是offsetHeight排序
     if (flowList[0].offsetHeight === 0) {
-      // 第一次：还没有加入任何内容，此时的顺序就是默认顺序
-      return flowListAry;
+      return flowListAry
     }
-    return flowListAry.sort((a, b) => a.offsetHeight - b.offsetHeight);
+    return flowListAry.sort((a, b) => a.offsetHeight - b.offsetHeight)
   };
 
-  // 每三个为一组，迭代所有的数据依次增加到页面中
-  let insertHTML = function (result) {
-    // result 就是从服务器获取的数据
+  // 绑定数据
+  let insertHTML = function (data) {
+    for (let i = 0; i < data.length; i += 3) {
+      let ary = [
+        data[i],
+        data[i + 1],
+        data[i + 2]
+      ];
 
-    for (let i = 0; i < result.length; i += 3) {
-      let item1 = result[i],
-        item2 = result[i + 1],
-        item3 = result[i + 2];
-
-      // 给li排序，并且向每个li中追加内容
+      // 给li按照其内容的高度排序
       let flowListAry = sortLi();
-      item1 ? flowListAry[0].innerHTML += queryHTML(item1) : null;
-      item2 ? flowListAry[1].innerHTML += queryHTML(item2) : null;
-      item3 ? flowListAry[2].innerHTML += queryHTML(item2) : null;
 
+      // 根据li的高度向其末尾追加内容
+      ary.forEach((item, index) => {
+        item
+          ? flowListAry[index].innerHTML += queryHMTL(item)
+          : null;
+      })
     }
-    isRun = false;
   };
 
-
-  // => 加载更多
+  // 加载更多
   let loadMore = function () {
     let winH = document.documentElement.clientHeight;
-    let pageH = document.documentElement.scrollHeight;
-    let scrollT = document.documentElement.scrollTop;
+    let pageH = document.documentElement.offsetHeight;
+    let winScrlTop = document.documentElement.scrollTop;
 
-    if ((pageH - winH + 100) <= (scrollT + 100) ) {
+    if ((pageH - winScrlTop - winH) <= 300) {
       queryData();
     }
   };
 
-  // => 懒加载
+  // 图片懒加载
   let lazyLoad = function () {
     let imgList = document.querySelectorAll('img');
-    let imgAry = Array.from(imgList);
-    imgAry.forEach(item => {
-      let imgOffsetTop = item.parentNode.offsetTop;
+
+    for (let i = 0; i < imgList.length; i++) {
+      let item = imgList[i];
+      let imgOffsetTop = item.offsetTop;
       let winH = document.documentElement.clientHeight;
-      let winScrollTop = document.documentElement.scrollTop;
-      let dataSrc = item.getAttribute('data-src');
-      if ((imgOffsetTop - winH - winScrollTop) <= -200 && !item.src) {
+      let pageSctp = document.documentElement.scrollTop;
+      if (imgOffsetTop - winH - pageSctp <= 100) {
+        // imgOffsetTop - winH - pageSctp === 0 说明恰好要出来了，如果小于0说明已经出来
         let newImg = document.createElement('img');
+        let dataSrc = item.getAttribute('data-src');
         newImg.src = dataSrc;
         newImg.onload = function () {
           item.src = dataSrc;
+          newImg = null; // 手动释放内存
         }
       }
-    })
+    }
   };
   return {
-    init: function () {
+    init () {
       queryData();
       lazyLoad();
-      window.onscroll = () => {
+      window.onscroll = function () {
         loadMore();
         lazyLoad();
-      };
+      }
     }
   }
 })();
